7c7d1a843f470f4c8629bde088f435bb4d3676fd
Minegamer95 <93994507+Minegamer95@users.noreply.github.com.>
2025-04-25 21:49:38 +0200

    ApplyChanges outgesourced

--- Minegamer95.YTPlaylistManager.Main/Services/PlaylistUpdater.cs
+++ Minegamer95.YTPlaylistManager.Main/Services/PlaylistUpdater.cs
@@ -1,6 +1,3 @@
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 using Minegamer95.YTPlaylistManager.Main.Model;
 using Minegamer95.YTPlaylistManager.Main.Services.Actions;
@@ -10,6 +7,8 @@ namespace Minegamer95.YTPlaylistManager.Main.Services;
 public class PlaylistUpdater
 {
   private readonly IPlaylistInteraction _playlistService;
+  private string _targetPlaylistId = string.Empty;
+  private bool _dryRun = false;
 
   public PlaylistUpdater(IPlaylistInteraction playlistService)
   {
@@ -20,7 +19,9 @@ public class PlaylistUpdater
     bool dryRun = false)
   {
     Console.WriteLine($"\nAktualisiere Playlist ID: {targetPlaylistId}...");
-    if (string.IsNullOrEmpty(targetPlaylistId))
+    _targetPlaylistId = targetPlaylistId;
+    _dryRun = dryRun;
+    if (string.IsNullOrEmpty(_targetPlaylistId))
     {
       await Console.Error.WriteLineAsync("Fehler: Keine Ziel-Playlist-ID angegeben.");
       return;
@@ -30,53 +31,24 @@ public class PlaylistUpdater
     {
       // --- Schritt 0: For now we assume that the target playlist can not contian duplicates ---
       desiredVideoIds = desiredVideoIds.Distinct().ToList();
-      
+
       // --- Schritt 1: Aktuellen Zustand lesen ---
       //Console.WriteLine(" Schritt 1: Lese aktuelle Playlist-Elemente...");
-      var currentItems = await _playlistService.ListItemsAsync(targetPlaylistId);
+      var currentItems = await _playlistService.ListItemsAsync(_targetPlaylistId);
       Console.WriteLine($" {currentItems.Count} Elemente in der Playlist gefunden.");
 
       // --- Schritt 2: Änderungsplan berechnen ---
       //Console.WriteLine(" Schritt 2: Berechne Änderungsplan...");
-      var changePlan = CalculateChangePlan(currentItems, desiredVideoIds, targetPlaylistId);
+      var changePlan = CalculateChangePlan(currentItems, desiredVideoIds, _targetPlaylistId);
       // LogChangePlan(changePlan); // Optional: Plan zur Übersicht ausgeben
 
       // --- Schritt 3: Aktionen ausführen ---
-      Console.WriteLine($" Schritt 4: Führe Aktionen aus (DryRun={dryRun})...");
-      var updateCost = changePlan.Actions.GetCost(_playlistService);
-      if (!QuotaManager.Instance.CanExecute(updateCost))
-      {
-        Console.WriteLine($"Quota für {changePlan.Actions.Count} Aktionen nicht ausreichend. Abbruch.");
-        return;
-      }
-      Console.WriteLine($"Kosten für die Aktionen: {updateCost} Quota-Einheiten");
-      Console.WriteLine($"Aktuelle Quota: {QuotaManager.Instance.RemainingPoints} Quota-Einheiten, Verbleibend nach Update: {QuotaManager.Instance.RemainingPoints - updateCost} Quota-Einheiten");
-      int actionCounter = 0;
-      var cursorPosStart = Console.GetCursorPosition();
-      var cursorPosEnd = Console.GetCursorPosition();
-      foreach (var action in changePlan.Actions)
-      {
-        actionCounter++;
-        if (cursorPosStart != cursorPosEnd)
-        {
-          Console.SetCursorPosition(cursorPosStart.Left, cursorPosStart.Top);
-          var range = Math.Abs(cursorPosEnd.Top - cursorPosStart.Top);
-          for (int i = 0; i < range; i++)
-            Console.Write(new string(' ', Console.WindowWidth));
-          Console.SetCursorPosition(cursorPosStart.Left, cursorPosStart.Top);
-        }
-        Console.WriteLine($"\n Aktion {actionCounter}/{changePlan.Actions.Count}: {action.Describe()}");
-        cursorPosEnd = Console.GetCursorPosition();
-        await action.ExecuteAsync(_playlistService, dryRun);
-      }
-
-      Console.WriteLine($"\nAktualisierung der Playlist {targetPlaylistId} abgeschlossen.");
+      await ApplyChanges(changePlan);
     }
     catch (Exception ex)
     {
       await Console.Error.WriteLineAsync(
-        $"Fehler beim Aktualisieren der Playlist {targetPlaylistId}: {ex.Message}\n{ex.StackTrace}");
-      // Gib mehr Details aus für die Fehlersuche
+        $"Fehler beim Aktualisieren der Playlist {_targetPlaylistId}");
     }
   }
 
@@ -102,10 +74,10 @@ public class PlaylistUpdater
         videosToDelete.Add(vItem);
         playlistActions.Add(new DeleteAction(vItem));
       }
-      simulator.RemoveByVideoId(item.Key!);
 
+      simulator.RemoveByVideoId(item.Key!);
     }
-    
+
     // --- Schritt 2: Fehlendes hinzufügen ---
     for (int i = 0; i < desiredVideoIds.Count; i++)
     {
@@ -115,14 +87,14 @@ public class PlaylistUpdater
       videosToInsert.Add(desiredVideoId, realPos); // videoId -> targetPosition
       playlistActions.Add(new InsertAction(desiredVideoId, realPos, playlistId));
     }
-    
+
     // --- Schritt 3: Vorhandene aktualisieren, falls sich Position geändert hat ---
     var getCurrentOrder = simulator.GetSimulatorItems();
     var updateOrder = groupedItems.OrderByDescending(x =>
     {
-      var video = getCurrentOrder.FirstOrDefault(y => 
+      var video = getCurrentOrder.FirstOrDefault(y =>
         y.Item.Snippet.ResourceId.VideoId == x.Key);
-      return video!.CurrentPositon - video.TargetPosition;
+      return Math.Abs(video!.CurrentPositon - video.TargetPosition);
     }).ToList();
     foreach (var item in updateOrder)
     {
@@ -145,7 +117,7 @@ public class PlaylistUpdater
         Id = video.Id,
         Snippet = new PlaylistItemSnippet
         {
-          PlaylistId = video.Snippet.PlaylistId, 
+          PlaylistId = video.Snippet.PlaylistId,
           ResourceId = video.Snippet.ResourceId,
           Position = realPos
         }
@@ -156,8 +128,49 @@ public class PlaylistUpdater
 
     return new PlaylistChangePlan(playlistId, videosToDelete, videosToUpdate, videosToInsert, playlistActions);
   }
-  
-  // Hilfsmethode zum Loggen des Change Plans (Optional)
+
+  private async Task ApplyChanges(PlaylistChangePlan changePlan)
+  {
+    if (changePlan.Actions.Count == 0)
+    {
+      Console.WriteLine($"Keine Änderungen an Playlist {_targetPlaylistId} erforderlich.");
+      return;
+    }
+
+    Console.WriteLine($" Schritt 4: Führe Aktionen aus (DryRun={_dryRun})...");
+    var updateCost = changePlan.Actions.GetCost(_playlistService);
+    if (!QuotaManager.Instance.CanExecute(updateCost))
+    {
+      Console.WriteLine($"Quota für {changePlan.Actions.Count} Aktionen nicht ausreichend. Abbruch.");
+      return;
+    }
+
+    Console.WriteLine($"Kosten für die Aktionen: {updateCost} Quota-Einheiten");
+    Console.WriteLine(
+      $"Aktuelle Quota: {QuotaManager.Instance.RemainingPoints} Quota-Einheiten, Verbleibend nach Update: {QuotaManager.Instance.RemainingPoints - updateCost} Quota-Einheiten");
+    var actionCounter = 0;
+    var cursorPosStart = Console.GetCursorPosition();
+    var cursorPosEnd = Console.GetCursorPosition();
+    foreach (var action in changePlan.Actions)
+    {
+      actionCounter++;
+      if (cursorPosStart != cursorPosEnd)
+      {
+        Console.SetCursorPosition(cursorPosStart.Left, cursorPosStart.Top);
+        var range = Math.Abs(cursorPosEnd.Top - cursorPosStart.Top);
+        for (int i = 0; i < range; i++)
+          Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(cursorPosStart.Left, cursorPosStart.Top);
+      }
+
+      Console.WriteLine($"\n Aktion {actionCounter}/{changePlan.Actions.Count}: {action.Describe()}");
+      cursorPosEnd = Console.GetCursorPosition();
+      await action.ExecuteAsync(_playlistService, _dryRun);
+    }
+
+    Console.WriteLine($"\nAktualisierung der Playlist {_targetPlaylistId} abgeschlossen.");
+  }
+
   private void LogChangePlan(PlaylistChangePlan plan)
   {
     Console.WriteLine("\n--- Berechneter Änderungsplan ---");
